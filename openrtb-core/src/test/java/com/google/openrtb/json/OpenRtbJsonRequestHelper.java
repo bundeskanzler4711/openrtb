78b10f5be34b6721651905533ad11c0f8a6c6e15
bundeskanzler4711 <master@sam6.de>
2016-06-02 09:48:35 +0200

    avoid line breaks if a setter's parameter fits in the same line, even if it's a complex expression

--- openrtb-core/src/test/java/com/google/openrtb/json/OpenRtbJsonRequestHelper.java
+++ openrtb-core/src/test/java/com/google/openrtb/json/OpenRtbJsonRequestHelper.java
@@ -135,44 +135,21 @@ class OpenRtbJsonRequestHelper {
     }
     impressionBuilder.setId("imp1").setBidfloor(100.0).setBidfloorcur("USD").setNative(nativeBuilder);
 
-    siteBuilder.setId("site1")
-        .setDomain("mysite.foo.com")
-        .setPage("http://mysite.foo.com/my/link")
-        .setMobile(false)
-        .setKeywords("my,key,words");
+    siteBuilder.setId("site1").setDomain("mysite.foo.com").setPage("http://mysite.foo.com/my/link").setMobile(false).setKeywords("my,key,words");
 
-    appBuilder.setId("app1")
-        .setName("my-app-name")
-        .setDomain("mysite.foo.com")
-        .setPaid(true)
-        .setKeywords("my,app,key,words");
+    appBuilder.setId("app1").setName("my-app-name").setDomain("mysite.foo.com").setPaid(true).setKeywords("my,app,key,words");
 
     geoBuilder.setCity("New York");
 
-    deviceBuilder.setUa(
-        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
-        .setGeo(geoBuilder)
-        .setDnt(false)
-        .setLmt(true)
-        .setIp("192.168.1.0")
-        .setIpv6("1:2:3:4:5:6:0:0");
+    deviceBuilder.setUa("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+        .setGeo(geoBuilder).setDnt(false).setLmt(true).setIp("192.168.1.0").setIpv6("1:2:3:4:5:6:0:0");
 
-    userBuilder.setId("user1")
-        .setBuyeruid("buyer1")
-        .setGender("O")
-        .setKeywords("user,builder,key,words")
-        .setGeo(geoBuilder);
+    userBuilder.setId("user1").setBuyeruid("buyer1").setGender("O").setKeywords("user,builder,key,words").setGeo(geoBuilder);
 
     bidRequestBuilder.setId("9zj61whbdl319sjgz098lpys5cngmtro_short_" + isRootNative + "_" + isNativeObject)
-        .addImp(impressionBuilder)
-        .setSite(siteBuilder)
-        .setApp(appBuilder)
-        .setDevice(deviceBuilder)
-        .setUser(userBuilder);
+        .addImp(impressionBuilder).setSite(siteBuilder).setApp(appBuilder).setDevice(deviceBuilder).setUser(userBuilder);
 
-    return openRtbJsonFactory
-        .newWriter()
-        .writeBidRequest(bidRequestBuilder.build());
+    return openRtbJsonFactory.newWriter().writeBidRequest(bidRequestBuilder.build());
   }
 
   private static String generateFullRequest(final boolean isRootNative, final boolean isNativeObject)
@@ -188,101 +165,30 @@ class OpenRtbJsonRequestHelper {
 
     final OpenRtbJsonFactory openRtbJsonFactory = OpenRtbJsonFactoryHelper.newJsonFactory(isRootNative, isNativeObject);
 
-    firstImpressionBuilder.setId("imp1")
-        .setBanner(OpenRtb.BidRequest.Imp.Banner.newBuilder()
-            .setWmax(300)
-            .setWmin(200)
-            .setHmax(100)
-            .setHmin(50)
-            .setId("banner1")
-            .setPos(
-                OpenRtb.BidRequest.Imp.AdPosition.ABOVE_THE_FOLD)
-            .addBtype(
-                OpenRtb.BidRequest.Imp.Banner.BannerAdType.JAVASCRIPT_AD)
-            .addBattr(OpenRtb.CreativeAttribute.TEXT_ONLY)
-            .addMimes("image/gif")
-            .setTopframe(true)
-            .addExpdir(
-                OpenRtb.BidRequest.Imp.Banner.ExpandableDirection.RIGHT)
-            .addApi(OpenRtb.BidRequest.Imp.APIFramework.MRAID_1)
-            .setExtension(TestExt.testBanner,
-                OpenRtbJsonFactoryHelper.test1))
-        .setDisplaymanager("dm1")
-        .setDisplaymanagerver("1.0")
-        .setInstl(false)
-        .setTagid("tag1")
-        .setBidfloor(100.0)
-        .setBidfloorcur("USD")
-        .setSecure(false)
-        .addIframebuster("buster1")
-        .setPmp(OpenRtb.BidRequest.Imp.Pmp.newBuilder()
-            .setPrivateAuction(false)
-            .addDeals(OpenRtb.BidRequest.Imp.Pmp.Deal.newBuilder()
-                .setId("deal1")
-                .setBidfloor(
-                    200.0)
-                .setBidfloorcur(
-                    "USD")
-                .addWseat(
-                    "seat2")
-                .addWadomain(
-                    "goodadv1")
-                .setAt(
-                    OpenRtb.BidRequest.AuctionType.SECOND_PRICE)
-                .setExtension(
-                    TestExt.testDeal,
-                    OpenRtbJsonFactoryHelper.test1))
-            .setExtension(TestExt.testPmp,
-                OpenRtbJsonFactoryHelper.test1))
+    firstImpressionBuilder.setId("imp1").setBanner(OpenRtb.BidRequest.Imp.Banner.newBuilder().setWmax(300)
+        .setWmin(200).setHmax(100).setHmin(50).setId("banner1").setPos(OpenRtb.BidRequest.Imp.AdPosition.ABOVE_THE_FOLD)
+        .addBtype(OpenRtb.BidRequest.Imp.Banner.BannerAdType.JAVASCRIPT_AD).addBattr(OpenRtb.CreativeAttribute.TEXT_ONLY)
+        .addMimes("image/gif").setTopframe(true).addExpdir(OpenRtb.BidRequest.Imp.Banner.ExpandableDirection.RIGHT)
+        .addApi(OpenRtb.BidRequest.Imp.APIFramework.MRAID_1).setExtension(TestExt.testBanner, OpenRtbJsonFactoryHelper.test1))
+        .setDisplaymanager("dm1").setDisplaymanagerver("1.0").setInstl(false).setTagid("tag1").setBidfloor(100.0)
+        .setBidfloorcur("USD").setSecure(false).addIframebuster("buster1").setPmp(OpenRtb.BidRequest.Imp.Pmp.newBuilder()
+        .setPrivateAuction(false).addDeals(OpenRtb.BidRequest.Imp.Pmp.Deal.newBuilder().setId("deal1").setBidfloor(200.0)
+            .setBidfloorcur("USD").addWseat("seat2").addWadomain("goodadv1").setAt(OpenRtb.BidRequest.AuctionType.SECOND_PRICE)
+            .setExtension(TestExt.testDeal, OpenRtbJsonFactoryHelper.test1)).setExtension(TestExt.testPmp, OpenRtbJsonFactoryHelper.test1))
         .setExtension(TestExt.testImp, OpenRtbJsonFactoryHelper.test1);
 
-    secondImpressionBuilder.setId("imp2")
-        .setVideo(OpenRtb.BidRequest.Imp.Video.newBuilder()
-            .addMimes("video/vp9")
-            .setLinearity(
-                OpenRtb.BidRequest.Imp.Video.VideoLinearity.LINEAR)
-            .setMinduration(15)
-            .setMaxduration(60)
-            .setProtocol(
-                OpenRtb.BidRequest.Imp.Video.VideoBidResponseProtocol.VAST_3_0)
-            .addProtocols(
-                OpenRtb.BidRequest.Imp.Video.VideoBidResponseProtocol.VAST_2_0)
-            .setW(200)
-            .setH(50)
-            .setStartdelay(0)
-            .setSequence(1)
-            .addBattr(OpenRtb.CreativeAttribute.TEXT_ONLY)
-            .setMaxextended(120)
-            .setMinbitrate(1000)
-            .setMaxbitrate(2000)
-            .setBoxingallowed(false)
-            .addPlaybackmethod(
-                OpenRtb.BidRequest.Imp.Video.VideoPlaybackMethod.CLICK_TO_PLAY)
-            .addDelivery(
-                OpenRtb.BidRequest.Imp.Video.ContentDeliveryMethod.STREAMING)
-            .setPos(
-                OpenRtb.BidRequest.Imp.AdPosition.ABOVE_THE_FOLD)
-            .addCompanionad(
-                OpenRtb.BidRequest.Imp.Banner.newBuilder()
-                    .setId("compad1")
-                    .setW(100)
-                    .setH(50))
-            .setCompanionad21(
-                OpenRtb.BidRequest.Imp.Video.CompanionAd.newBuilder()
-                    .addBanner(
-                        OpenRtb.BidRequest.Imp.Banner
-                            .newBuilder()
-                            .setId(
-                                "compad2")
-                            .setW(
-                                110)
-                            .setH(
-                                60)))
-            .addApi(OpenRtb.BidRequest.Imp.APIFramework.VPAID_2)
-            .addCompaniontype(
-                OpenRtb.BidRequest.Imp.Video.VASTCompanionType.HTML)
-            .setExtension(TestExt.testVideo,
-                OpenRtbJsonFactoryHelper.test1));
+    secondImpressionBuilder.setId("imp2").setVideo(OpenRtb.BidRequest.Imp.Video.newBuilder().addMimes("video/vp9")
+        .setLinearity(OpenRtb.BidRequest.Imp.Video.VideoLinearity.LINEAR).setMinduration(15).setMaxduration(60)
+        .setProtocol(OpenRtb.BidRequest.Imp.Video.VideoBidResponseProtocol.VAST_3_0)
+        .addProtocols(OpenRtb.BidRequest.Imp.Video.VideoBidResponseProtocol.VAST_2_0).setW(200).setH(50).setStartdelay(0)
+        .setSequence(1).addBattr(OpenRtb.CreativeAttribute.TEXT_ONLY).setMaxextended(120).setMinbitrate(1000)
+        .setMaxbitrate(2000).setBoxingallowed(false).addPlaybackmethod(OpenRtb.BidRequest.Imp.Video.VideoPlaybackMethod.CLICK_TO_PLAY)
+        .addDelivery(OpenRtb.BidRequest.Imp.Video.ContentDeliveryMethod.STREAMING)
+        .setPos(OpenRtb.BidRequest.Imp.AdPosition.ABOVE_THE_FOLD).addCompanionad(OpenRtb.BidRequest.Imp.Banner.newBuilder()
+            .setId("compad1").setW(100).setH(50)).setCompanionad21(OpenRtb.BidRequest.Imp.Video.CompanionAd.newBuilder()
+            .addBanner(OpenRtb.BidRequest.Imp.Banner.newBuilder().setId("compad2").setW(110).setH(60)))
+        .addApi(OpenRtb.BidRequest.Imp.APIFramework.VPAID_2).addCompaniontype(OpenRtb.BidRequest.Imp.Video.VASTCompanionType.HTML)
+        .setExtension(TestExt.testVideo, OpenRtbJsonFactoryHelper.test1));
 
     if (isNativeObject) {
       nativeBuilder.setRequestNative(generateNativeRequest());
@@ -292,108 +198,44 @@ class OpenRtbJsonRequestHelper {
           .writeNativeRequest(generateNativeRequest().build()));
     }
 
-    nativeBuilder.setVer("1.0")
-        .addApi(OpenRtb.BidRequest.Imp.APIFramework.MRAID_1)
-        .addBattr(OpenRtb.CreativeAttribute.TEXT_ONLY)
+    nativeBuilder.setVer("1.0").addApi(OpenRtb.BidRequest.Imp.APIFramework.MRAID_1).addBattr(OpenRtb.CreativeAttribute.TEXT_ONLY)
         .setExtension(TestExt.testNative, OpenRtbJsonFactoryHelper.test1);
 
-    thirdImpressionBuilder.setId("imp3")
-        .setNative(nativeBuilder)
-        .setExtension(TestExt.testImp, OpenRtbJsonFactoryHelper.test1);
+    thirdImpressionBuilder.setId("imp3").setNative(nativeBuilder).setExtension(TestExt.testImp, OpenRtbJsonFactoryHelper.test1);
 
-    deviceBuilder.setUa("Chrome")
-        .setGeo(OpenRtb.BidRequest.Geo.newBuilder()
-            .setLat(90.0)
-            .setLon(45.0)
-            .setType(OpenRtb.BidRequest.Geo.LocationType.GPS_LOCATION)
-            .setCountry("USA")
-            .setRegion("New York")
-            .setRegionfips104("US36")
-            .setMetro("New York")
-            .setCity("New York City")
-            .setZip("10000")
-            .setUtcoffset(3600)
-            .setExtension(TestExt.testGeo, OpenRtbJsonFactoryHelper.test1))
-        .setDnt(false)
-        .setLmt(false)
-        .setIp("192.168.1.0")
-        .setIpv6("1:2:3:4:5:6:0:0")
-        .setDevicetype(OpenRtb.BidRequest.Device.DeviceType.MOBILE)
-        .setMake("Motorola")
-        .setModel("MotoX")
-        .setOs("Android")
-        .setOsv("3.2.1")
-        .setHwv("X")
-        .setW(640)
-        .setH(1024)
-        .setPpi(300)
-        .setPxratio(1.0)
-        .setJs(true)
-        .setFlashver("11")
-        .setLanguage("en")
-        .setCarrier("77777")
-        .setConnectiontype(OpenRtb.BidRequest.Device.ConnectionType.CELL_4G)
-        .setIfa("999")
-        .setDidsha1("1234")
-        .setDidmd5("4321")
-        .setDpidsha1("5678")
-        .setDpidmd5("8765")
-        .setMacsha1("abc")
-        .setMacmd5("xyz")
-        .setExtension(TestExt.testDevice, OpenRtbJsonFactoryHelper.test1);
+    deviceBuilder.setUa("Chrome").setGeo(OpenRtb.BidRequest.Geo.newBuilder().setLat(90.0).setLon(45.0)
+        .setType(OpenRtb.BidRequest.Geo.LocationType.GPS_LOCATION).setCountry("USA").setRegion("New York")
+        .setRegionfips104("US36").setMetro("New York").setCity("New York City").setZip("10000").setUtcoffset(3600)
+        .setExtension(TestExt.testGeo, OpenRtbJsonFactoryHelper.test1)).setDnt(false).setLmt(false).setIp("192.168.1.0")
+        .setIpv6("1:2:3:4:5:6:0:0").setDevicetype(OpenRtb.BidRequest.Device.DeviceType.MOBILE).setMake("Motorola")
+        .setModel("MotoX").setOs("Android").setOsv("3.2.1").setHwv("X").setW(640).setH(1024).setPpi(300).setPxratio(1.0)
+        .setJs(true).setFlashver("11").setLanguage("en").setCarrier("77777").setConnectiontype(OpenRtb.BidRequest.Device
+        .ConnectionType.CELL_4G).setIfa("999").setDidsha1("1234").setDidmd5("4321").setDpidsha1("5678").setDpidmd5("8765")
+        .setMacsha1("abc").setMacmd5("xyz").setExtension(TestExt.testDevice, OpenRtbJsonFactoryHelper.test1);
 
-    userBuilder.setId("user1")
-        .setBuyeruid("Picard")
-        .setYob(1973)
-        .setGender("M")
-        .setKeywords("boldly,going")
-        .setCustomdata("data1")
-        .setGeo(OpenRtb.BidRequest.Geo.newBuilder().setZip("12345"))
-        .addData(OpenRtb.BidRequest.Data.newBuilder()
-            .setId("data1")
-            .setName("dataname1")
-            .addSegment(OpenRtb.BidRequest.Data.Segment.newBuilder()
-                .setId("seg1")
-                .setName("segname1")
-                .setValue("segval1")
-                .setExtension(
-                    TestExt.testSegment,
-                    OpenRtbJsonFactoryHelper.test1))
+    userBuilder.setId("user1").setBuyeruid("Picard").setYob(1973).setGender("M").setKeywords("boldly,going")
+        .setCustomdata("data1").setGeo(OpenRtb.BidRequest.Geo.newBuilder().setZip("12345"))
+        .addData(OpenRtb.BidRequest.Data.newBuilder().setId("data1").setName("dataname1")
+            .addSegment(OpenRtb.BidRequest.Data.Segment.newBuilder().setId("seg1").setName("segname1")
+                .setValue("segval1").setExtension(TestExt.testSegment, OpenRtbJsonFactoryHelper.test1))
             .setExtension(TestExt.testData, OpenRtbJsonFactoryHelper.test1))
         .setExtension(TestExt.testUser, OpenRtbJsonFactoryHelper.test1);
 
     regsBuilder.setCoppa(true).setExtension(TestExt.testRegs, OpenRtbJsonFactoryHelper.test1);
 
     bidRequestBuilder.setId("9zj61whbdl319sjgz098lpys5cngmtro_full_" + isRootNative + "_" + isNativeObject)
-        .addImp(firstImpressionBuilder)
-        .addImp(secondImpressionBuilder)
-        .addImp(thirdImpressionBuilder)
-        .setDevice(deviceBuilder)
-        .setUser(userBuilder)
-        .setAt(OpenRtb.BidRequest.AuctionType.SECOND_PRICE)
-        .setTmax(100)
-        .addWseat("seat1")
-        .setAllimps(false)
-        .addCur("USD")
-        .addAllBcat(asList("IAB11", "IAB11-4"))
-        .addBadv("badguy")
-        .setRegs(regsBuilder)
-        .setTest(false)
+        .addImp(firstImpressionBuilder).addImp(secondImpressionBuilder).addImp(thirdImpressionBuilder).setDevice(deviceBuilder)
+        .setUser(userBuilder).setAt(OpenRtb.BidRequest.AuctionType.SECOND_PRICE).setTmax(100).addWseat("seat1").setAllimps(false)
+        .addCur("USD").addAllBcat(asList("IAB11", "IAB11-4")).addBadv("badguy").setRegs(regsBuilder).setTest(false)
         .setExtension(TestExt.testRequest2, OpenRtbJsonFactoryHelper.test2)
         .setExtension(TestExt.testRequest1, OpenRtbJsonFactoryHelper.test1);
 
-    return openRtbJsonFactory
-        .newWriter()
-        .writeBidRequest(bidRequestBuilder.build());
+    return openRtbJsonFactory.newWriter().writeBidRequest(bidRequestBuilder.build());
   }
 
   private static OpenRtb.NativeRequest.Builder generateNativeRequest() throws IOException {
     final OpenRtb.NativeRequest.Builder nativeRequest = OpenRtb.NativeRequest.newBuilder();
-    nativeRequest.setVer("1")
-        .setLayout(OpenRtb.NativeRequest.LayoutId.APP_WALL)
-        .setAdunit(OpenRtb.NativeRequest.AdUnitId.IAB_IN_AD_NATIVE)
-        .setPlcmtcnt(1)
-        .setSeq(1);
+    nativeRequest.setVer("1").setLayout(OpenRtb.NativeRequest.LayoutId.APP_WALL).setAdunit(OpenRtb.NativeRequest.AdUnitId.IAB_IN_AD_NATIVE).setPlcmtcnt(1).setSeq(1);
     return nativeRequest;
   }
 
--- openrtb-core/src/test/java/com/google/openrtb/json/OpenRtbJsonResponseHelper.java
+++ openrtb-core/src/test/java/com/google/openrtb/json/OpenRtbJsonResponseHelper.java
@@ -120,47 +120,24 @@ class OpenRtbJsonResponseHelper {
 
     final OpenRtbJsonFactory openRtbJsonFactory = OpenRtbJsonFactoryHelper.newJsonFactory(isRootNative, isNativeObject);
 
-    seatBidBuilder.setId("bid")
-        .setImpid("imp")
-        .setPrice(19.95)
-        .setAdid("adid")
-        .setNurl("http://iwon.com")
-        .addAdomain("http://myads.com")
-        .setIurl("http://mycdn.com/ad.gif")
-        .setCid("cid")
-        .setCrid("crid")
-        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY)
-        .setDealid("deal")
-        .setW(100)
-        .setH(80)
-        .setBundle("com.google.testapp")
-        .addCat("IAB10-2")
-        .setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
+    seatBidBuilder.setId("bid").setImpid("imp").setPrice(19.95).setAdid("adid").setNurl("http://iwon.com")
+        .addAdomain("http://myads.com").setIurl("http://mycdn.com/ad.gif").setCid("cid").setCrid("crid")
+        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY).setDealid("deal").setW(100).setH(80).setBundle("com.google.testapp")
+        .addCat("IAB10-2").setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
 
-    nativeResponseBuilder.setVer("1.0")
-        .setLink(OpenRtb.NativeResponse.Link.newBuilder())
-        .addImptrackers("http://my.imp.tracker");
+    nativeResponseBuilder.setVer("1.0").setLink(OpenRtb.NativeResponse.Link.newBuilder()).addImptrackers("http://my.imp.tracker");
 
     if (isNativeObject) {
       seatBidBuilder.setAdmNative(nativeResponseBuilder);
     } else {
-      seatBidBuilder.setAdm(openRtbJsonFactory
-          .newNativeWriter()
-          .writeNativeResponse(nativeResponseBuilder.build()));
+      seatBidBuilder.setAdm(openRtbJsonFactory.newNativeWriter().writeNativeResponse(nativeResponseBuilder.build()));
     }
 
     seatBuilder.addBid(seatBidBuilder).setSeat("seat");
 
-    bidResponseBuilder.setId("resp")
-        .addSeatbid(seatBuilder)
-        .setBidid("bid")
-        .setCur("USD")
-        .setCustomdata("mydata")
-        .setNbr(OpenRtb.BidResponse.NoBidReason.TECHNICAL_ERROR);
+    bidResponseBuilder.setId("resp").addSeatbid(seatBuilder).setBidid("bid").setCur("USD").setCustomdata("mydata").setNbr(OpenRtb.BidResponse.NoBidReason.TECHNICAL_ERROR);
 
-    return openRtbJsonFactory
-        .newWriter()
-        .writeBidResponse(bidResponseBuilder.build());
+    return openRtbJsonFactory.newWriter().writeBidResponse(bidResponseBuilder.build());
 
   }
 
@@ -183,115 +160,52 @@ class OpenRtbJsonResponseHelper {
 
     final OpenRtbJsonFactory openRtbJsonFactory = OpenRtbJsonFactoryHelper.newJsonFactory(isRootNative, isNativeObject);
 
-    firstSeatBidBuilder.setId("bid1")
-        .setImpid("imp1")
-        .setPrice(19.95)
-        .setAdid("adid1")
-        .setNurl("http://iwon.com")
-        .addAdomain("http://myads.com")
-        .setIurl("http://mycdn.com/ad.gif")
-        .setCid("cid1")
-        .setCrid("crid1")
-        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY)
-        .setDealid("deal1")
-        .setW(100)
-        .setH(80)
-        .setBundle("com.google.testapp")
-        .addCat("IAB10-2")
-        .setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
+    firstSeatBidBuilder.setId("bid1").setImpid("imp1").setPrice(19.95).setAdid("adid1").setNurl("http://iwon.com")
+        .addAdomain("http://myads.com").setIurl("http://mycdn.com/ad.gif").setCid("cid1").setCrid("crid1")
+        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY).setDealid("deal1").setW(100).setH(80).setBundle("com.google.testapp")
+        .addCat("IAB10-2").setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
 
-    firstNativeResponseBuilder.setVer("1.0")
-        .setLink(OpenRtb.NativeResponse.Link.newBuilder())
-        .addImptrackers("http://my.first.imp.tracker");
+    firstNativeResponseBuilder.setVer("1.0").setLink(OpenRtb.NativeResponse.Link.newBuilder()).addImptrackers("http://my.first.imp.tracker");
 
-    secondSeatBidBuilder.setId("bid2")
-        .setImpid("imp2")
-        .setPrice(19.95)
-        .setAdid("adid2")
-        .setNurl("http://iwon.com")
-        .addAdomain("http://myads.com")
-        .setIurl("http://mycdn.com/ad.gif")
-        .setCid("cid2")
-        .setCrid("crid2")
-        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY)
-        .setDealid("deal2")
-        .setW(100)
-        .setH(80)
-        .setBundle("com.google.testapp")
-        .addCat("IAB10-2")
-        .setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
+    secondSeatBidBuilder.setId("bid2").setImpid("imp2").setPrice(19.95).setAdid("adid2").setNurl("http://iwon.com")
+        .addAdomain("http://myads.com").setIurl("http://mycdn.com/ad.gif").setCid("cid2").setCrid("crid2")
+        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY).setDealid("deal2").setW(100).setH(80).setBundle("com.google.testapp")
+        .addCat("IAB10-2").setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
 
-    secondNativeResponseBuilder.setVer("2.0")
-        .setLink(OpenRtb.NativeResponse.Link.newBuilder())
-        .addImptrackers("http://my.first.imp.tracker");
+    secondNativeResponseBuilder.setVer("2.0").setLink(OpenRtb.NativeResponse.Link.newBuilder()).addImptrackers("http://my.first.imp.tracker");
 
-    thirdSeatBidBuilder.setId("bid2")
-        .setImpid("imp3")
-        .setPrice(19.95)
-        .setAdid("adid3")
-        .setNurl("http://iwon.com")
-        .addAdomain("http://myads.com")
-        .setIurl("http://mycdn.com/ad.gif")
-        .setCid("cid3")
-        .setCrid("crid3")
-        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY)
-        .setDealid("deal3")
-        .setW(100)
-        .setH(80)
-        .setBundle("com.google.testapp")
-        .addCat("IAB10-2")
-        .setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
+    thirdSeatBidBuilder.setId("bid2").setImpid("imp3").setPrice(19.95).setAdid("adid3").setNurl("http://iwon.com")
+        .addAdomain("http://myads.com").setIurl("http://mycdn.com/ad.gif").setCid("cid3").setCrid("crid3")
+        .addAttr(OpenRtb.CreativeAttribute.TEXT_ONLY).setDealid("deal3").setW(100).setH(80).setBundle("com.google.testapp")
+        .addCat("IAB10-2").setExtension(TestExt.testBid, OpenRtbJsonFactoryHelper.test1);
 
-    thirdNativeResponseBuilder.setVer("3.0")
-        .setLink(OpenRtb.NativeResponse.Link.newBuilder())
-        .addImptrackers("http://my.third.imp.tracker");
+    thirdNativeResponseBuilder.setVer("3.0").setLink(OpenRtb.NativeResponse.Link.newBuilder()).addImptrackers("http://my.third.imp.tracker");
 
     if (isNativeObject) {
       firstSeatBidBuilder.setAdmNative(firstNativeResponseBuilder);
       secondSeatBidBuilder.setAdmNative(secondNativeResponseBuilder);
     } else {
-      firstSeatBidBuilder.setAdm(openRtbJsonFactory
-          .newNativeWriter()
-          .writeNativeResponse(firstNativeResponseBuilder.build()));
-      secondSeatBidBuilder.setAdm(openRtbJsonFactory
-          .newNativeWriter()
-          .writeNativeResponse(secondNativeResponseBuilder.build()));
+      firstSeatBidBuilder.setAdm(openRtbJsonFactory.newNativeWriter().writeNativeResponse(firstNativeResponseBuilder.build()));
+      secondSeatBidBuilder.setAdm(openRtbJsonFactory.newNativeWriter().writeNativeResponse(secondNativeResponseBuilder.build()));
     }
 
-    firstSeatBuilder.addBid(firstSeatBidBuilder)
-        .setSeat("seat1")
-        .setGroup(false)
-        .setExtension(TestExt.testSeat, OpenRtbJsonFactoryHelper.test1);
+    firstSeatBuilder.addBid(firstSeatBidBuilder).setSeat("seat1").setGroup(false).setExtension(TestExt.testSeat, OpenRtbJsonFactoryHelper.test1);
 
     seatBidArrayList.add(secondSeatBidBuilder.build());
     seatBidArrayList.add(thirdSeatBidBuilder.build());
 
-    secondSeatBuilder.addAllBid(seatBidArrayList)
-        .setSeat("seat2")
-        .setGroup(true)
-        .setExtension(TestExt.testSeat, OpenRtbJsonFactoryHelper.test1);
+    secondSeatBuilder.addAllBid(seatBidArrayList).setSeat("seat2").setGroup(true).setExtension(TestExt.testSeat, OpenRtbJsonFactoryHelper.test1);
 
     seatArrayList.add(firstSeatBuilder.build());
     seatArrayList.add(secondSeatBuilder.build());
 
-    bidResponseBuilder.setId("resp1")
-        .addAllSeatbid(seatArrayList)
-        .setBidid("bid1")
-        .setCur("USD")
-        .setCustomdata("mydata")
-        .setNbr(OpenRtb.BidResponse.NoBidReason.TECHNICAL_ERROR)
-        .setExtension(TestExt.testResponse1, OpenRtbJsonFactoryHelper.test1)
-        .addExtension(TestExt.testResponse2, OpenRtbJsonFactoryHelper.test2)
-        .addExtension(TestExt.testResponse2, OpenRtbJsonFactoryHelper.test2)
-        .setExtension(TestExt.testResponse2A, OpenRtbJsonFactoryHelper.test2)
-        .setExtension(TestExt.testResponse2B, OpenRtbJsonFactoryHelper.test2)
-        .setExtension(TestExt.testResponse3, 99)
-        .addExtension(TestExt.testResponse4, 10)
-        .addExtension(TestExt.testResponse4, 20);
+    bidResponseBuilder.setId("resp1").addAllSeatbid(seatArrayList).setBidid("bid1").setCur("USD").setCustomdata("mydata")
+        .setNbr(OpenRtb.BidResponse.NoBidReason.TECHNICAL_ERROR).setExtension(TestExt.testResponse1, OpenRtbJsonFactoryHelper.test1)
+        .addExtension(TestExt.testResponse2, OpenRtbJsonFactoryHelper.test2).addExtension(TestExt.testResponse2, OpenRtbJsonFactoryHelper.test2)
+        .setExtension(TestExt.testResponse2A, OpenRtbJsonFactoryHelper.test2).setExtension(TestExt.testResponse2B, OpenRtbJsonFactoryHelper.test2)
+        .setExtension(TestExt.testResponse3, 99).addExtension(TestExt.testResponse4, 10).addExtension(TestExt.testResponse4, 20);
 
-    return openRtbJsonFactory
-        .newWriter()
-        .writeBidResponse(bidResponseBuilder.build());
+    return openRtbJsonFactory.newWriter().writeBidResponse(bidResponseBuilder.build());
   }
 
 }
